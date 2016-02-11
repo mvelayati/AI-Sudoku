c499efaf7a506fa043a4990227eee7f8d69da396
Daniel Ching <daniel.j.ching@gmail.com>
2016-02-10 16:40:09 -0800

    Started the forward_prop method

--- Solver.py
+++ Solver.py
@@ -7,7 +7,6 @@ class Solver(object):
     """docstring for Solver. The Solver object solves Sudoku puzzles using a tunable level of complexity."""
     def __init__(self):
         self.num_backtracks = 0 # The number of times the solver needed to undo a value assignment
-        self.board = None # holds the game board
         self.solution = [] # list that holds snap shots of the game each time a value is assigned
         self.time = float('inf'); # The total amount of time it takes to solve a problem
 
@@ -20,14 +19,14 @@ class Solver(object):
         timelimit = self.time + 300
         self.num_backtracks = 0
 
-        self.solution.push(copy(Sudoku))
+        self.solution.append(deepcopy(Sudoku))
 
         is_solved = solve_recurse(level, order_method, timelimit)
 
-        return [is_solved, self.num_backtracks, time.process_time() - self.time, self.solution.front()]
+        return [is_solved, self.num_backtracks, time.process_time() - self.time, self.solution[-1]]
 
     def solve_recurse(level, order_method, timelimit):
-        Sudoku = copy(self.solution.front())
+        Sudoku = deepcopy(self.solution.front())
         is_solved = Sudoku.is_solved() # Check if already solution
 
         while(not is_solved and time.process_time() < timelimit):
@@ -38,8 +37,8 @@ class Solver(object):
                 Sudoku.assign(spot, possibility)
 
                 if Sudoku.forward_prop(level, spot):
-                    # save a copy of puzzle each time assignment isn't illegal
-                    self.solution.push(copy(Sudoku))
+                    # save a deepcopy of puzzle each time assignment isn't illegal
+                    self.solution.append(deepcopy(Sudoku))
 
                     # move to the next assignment
                     if solve_recurse(level, order_method, timelimit):
@@ -49,8 +48,8 @@ class Solver(object):
                         # this assignment did not lead to a solution
                         self.solution.pop()
 
-                # made to the end of this for loop; you are backtracking
+                # made it to the end of this for loop; you are backtracking
                 self.num_backtracks += 1 # continue searching possibilities for valid propogations
-                Sudoku = copy(self.solution.front()) # reset the puzzle to last partial solution
+                Sudoku = deepcopy(self.solution[-1]) # reset the puzzle to last partial solution
 
         return is_solved
--- Sudoku.py
+++ Sudoku.py
@@ -16,12 +16,59 @@ class Sudoku(object):
         is_valid = True
 
         if k >= 3:
-            None # do something
+            tuples(3,start.row())
+            tuples(3,start.row())
         if k >= 2:
-            None # do something
+            tuples(2,start.row())
+            tuples(3,start.row())
         if k >= 1:
-            None # do something
+            for spot in start.neighborhood():
+                if not spot.is_assigned():
+                    domain = 0
+                    for r in spot.row():
+                        domain = domain | r.domain
+                    domain = (~domain & spot.domain)
+                    if bin(domain).count('1') == 1
+                        assign(spot, domain)
+                if not spot.is_assigned():
+                    domain = 0
+                    for c in spot.col():
+                        domain = domain | c.domain
+                    domain = (~domain & spot.domain)
+                    if bin(domain).count('1') == 1
+                        assign(spot, domain)
+                if not spot.is_assigned():
+                    domain = 0
+                    for b in spot.box():
+                        domain = domain | b.domain
+                    domain = (~domain & spot.domain)
+                    if bin(domain).count('1') == 1
+                        assign(spot, domain)
+
         if k >= 0:
-            None # do something
+            for spot in start.neighborhood():
+                if not spot.is_assigned() and spot.domain_size() == 1:
+                    self.assign(spot,spot.domain)
+                    forward_prop(k, spot)
 
         return is_valid
+    def tuples(k,set)
+        maybe_matches = []
+        for spot in set:
+            # find all domains size 2
+            bin(spot.domain).count('1') == k
+            maybe_matches.append(spot)
+
+        # see if there are any pairs among the domains
+        while len(maybe_matches) > 1:
+            matches = [0]
+            for i = 1:len(maybe_matches):
+                if maybe_matches[0].domain == maybe_matches[i].domain:
+                    matches.append(i)
+            if len(matches) == k:
+                #remove the domains of these k from their neighborhoods
+                allneighborsdomains & ~thesematchesdomains
+
+        for spot in start.col():
+
+        for spot in start.box():
